f4e3a16850c3bb330cb93ef0d8672f148581b514
anrikawamo02-hash <anrikawamo02@gmail.com>
2026-07-16 20:54:19 +0900

    language.js を更新

--- language.js
+++ language.js
@@ -2,8 +2,16 @@ const translations = {
   JP: {
     roomSelect: "号室選択"
   },
-  EN: {},
-  ES: {},
-  KO: {},
-  ZH: {}
+  EN: {
+    roomSelect: "ROOM SELECT"
+  },
+  ES: {
+    roomSelect: "SELECCIONAR HABITACIÓN"
+  },
+  KO: {
+    roomSelect: "객실 선택"
+  },
+  ZH: {
+    roomSelect: "选择房间"
+  }
 };
